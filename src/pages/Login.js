bdda6a02459263339db4e9e6109cb324ca4e66ba
kkcom284 <kkcom9100@gmail.com>
2024-02-04 22:46:09 +0900

    chat App

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password).catch((err) => {
+      console.log(err);
+    });
+  };
+
   return (
     <>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">E-mail</label>
-          <input type="email" id="email" name="email" placeholder="Email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
@@ -16,6 +36,10 @@ const Login = () => {
             id="password"
             name="password"
             placeholder="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
         </div>
         <button type="submit">Login</button>
