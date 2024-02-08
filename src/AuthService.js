85f8df584588682f94d0f04e72faa30b940dc1f9
kkcom284 <kkcom9100@gmail.com>
2024-02-08 20:31:57 +0900

    chat App

--- src/AuthService.js
+++ src/AuthService.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./config/firebase";
 
-const AuthProvider = () => {
+const AuthContext = React.createContext();
+
+const AuthProvider = (children) => {
   const [user, setUser] = useState(null);
-  onAuthStateChanged(auth, (user) => {
-    setUser(user);
-  });
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, []);
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
 };
+
+export { AuthContext, AuthProvider };
--- src/pages/App.js
+++ src/pages/App.js
@@ -6,15 +6,18 @@ import Login from "./Login";
 import SignUp from "./SignUp";
 import Room from "./Room";
 
+import AuthProvider from "./AuthService";
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Room />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Room />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 // <>
